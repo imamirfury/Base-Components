a18a7bbbf838edcc1e81429f33e3130d214d111f
Amir_Fury <amir_fury@Amir-Furys-MacBook-Pro.local>
2021-12-17 11:46:55 +0530

    initial commit

--- app/src/main/java/com/fury/basecomponents/MainActivity.kt
+++ app/src/main/java/com/fury/basecomponents/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import com.fury.basecomponents.databinding.ActivityMainBinding
 import com.furybase.base.FuryBaseActivity
-import com.furybase.extension.fetchAllCountriesFromAssets
-import com.furybase.utils.FuryDatePicker
 
 class MainActivity : FuryBaseActivity<ActivityMainBinding>() {
 
-    val countries = fetchAllCountriesFromAssets()
+
 
     override val layoutRes: Int
         get() = R.layout.activity_main
