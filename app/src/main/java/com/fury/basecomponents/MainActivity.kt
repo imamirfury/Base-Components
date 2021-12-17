253844e615b195cba4d898320aab1d2a50cea8a4
Amir_Fury <amir_fury@Amir-Furys-MacBook-Pro.local>
2021-12-17 11:56:15 +0530

    initial commit

--- app/src/main/java/com/fury/basecomponents/MainActivity.kt
+++ app/src/main/java/com/fury/basecomponents/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.fury.basecomponents
 
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-import com.fury.basecomponents.databinding.ActivityMainBinding
-import com.furybase.base.FuryBaseActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : FuryBaseActivity<ActivityMainBinding>() {
+class MainActivity : AppCompatActivity() {
 
 
-
-    override val layoutRes: Int
-        get() = R.layout.activity_main
-
-    override fun getToolbar(binding: ActivityMainBinding): Toolbar? = null
-
-    override fun onActivityReady(instanceState: Bundle?, binding: ActivityMainBinding) {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 }
